eca3e47aa6be939af911ed530491602a62acc541
jace <jacegrimes@gmail.com>
2023-03-07 21:18:33 -0600

    Updates to register, login and updates to search

--- App.js
+++ App.js
@@ -5,3 +5,7 @@ import AppContainer from "./src/navigations/AppNavigation";
 export default function App() {
   return <AppContainer />;
 }
+
+
+//   git config --global user.email "you@example.com"
+//    git config --global user.name "Your Name"
