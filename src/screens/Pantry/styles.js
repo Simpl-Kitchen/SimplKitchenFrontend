472577badadce0449c8d641b0f23da3113e93d5c
Scott Houghton <94723197+EdwardHoughton@users.noreply.github.com>
2023-03-27 17:40:00 -0500

    Image Updated

--- src/screens/Pantry/styles.js
+++ src/screens/Pantry/styles.js
@@ -4,39 +4,68 @@
 // needs style for list of items in pantry
 // needs style for categories of items in pantry
 
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  searchButton: {
+    backgroundColor: '#0099ff',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  searchButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  carouselContainer: {
+    height: 300,
+  },
+  slide: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    image: {
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        marginBottom: 20,
-    },
-    name: {
-        fontSize: 28,
-        fontWeight: "bold",
-    },
-    
-    button: {
-        backgroundColor: "#FF6347",
-        padding: 10,
-        borderRadius: 10,
-        alignItems: "center",
-        marginTop: 30,
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: 20,
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  photo: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+  },
 });
 
 export default styles;
+
 
 
