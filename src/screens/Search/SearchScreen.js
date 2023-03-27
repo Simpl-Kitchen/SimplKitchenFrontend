59daaf528c5f949d3fd0a1fd9c7d7ef34b252ac7
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-03-27 17:45:36 -0500

    Updated search functionality with rslts displaying

--- src/screens/Search/SearchScreen.js
+++ src/screens/Search/SearchScreen.js
@@ -1,4 +1,16 @@
-import React, { useState, useEffect, Component } from "react";
+// needs to get ingredients from the database
+// needs to display ingredients in a list
+// needs to have a button to add ingredients
+// needs to have a button to remove ingredients
+// needs to have categories for ingredients (produce, dairy, meat, etc.)
+// needs to haev a preset category for ingredients
+// search function for ingredients at the top of screen and barcode scanner
+// needs to have a button to add ingredients to the shopping list
+// needs to have a button to add ingredients to the pantry
+// needs to have authorization
+// Dan needs you to add some "Token" thing
+
+import React, { useState } from "react";
 import axios from "axios";
 import {
   View,
@@ -7,28 +19,18 @@ import {
   TextInput,
   Button,
   TouchableHighlight,
+  Image,
 } from "react-native";
 
 import styles from "./styles";
-import token from "../Register/RegisterScreen";
 
-// if (token = null){
-//   navigation.navigate("Login");
-// }
+const PantryScreen = ({ navigation }) => {
+  const [search, setSearch] = useState("");
+  const [ingredients, setIngredients] = useState([]);
 
-class SearchScreen extends Component {
-  state = {
-    search: "",
-    recipes: [],
-  };
-
-  // axios get request for ingredient entered by user
-
-  // axios call to spoonacular recipe db api
-
-  fetchData = async () => {
+  const fetchData = async () => {
     try {
-      const queryObject = { search: this.state.search };
+      const queryObject = { search };
       const options = {
         method: "GET",
         url: "https://simplkitchenapi.onrender.com/api/v1/search/ingredients",
@@ -38,69 +40,104 @@ class SearchScreen extends Component {
       const results = response.data.foodData.results;
 
       // Append image url to image
-      results.forEach((item, index) => {
-
-        item.image = " https://spoonacular.com/cdn/ingredients_100x100/" + item.image
-        console.log(
-          `${index + 1}. ID: ${item.id}, Name: ${item.name}, Image: ${
-            item.image
-          }`
-        );
+      results.forEach((item) => {
+        item.image = `https://spoonacular.com/cdn/ingredients_100x100/${item.image}`;
       });
 
-      // Loop through the results array and display the properties in a cleaner format
-      results.forEach((item, index) => {
-        console.log(
-          `${index + 1}. ID: ${item.id}, Name: ${item.name}, Image: ${
-            item.image
-          }`
-        );
-      });
+      setIngredients(results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // render ingredients using the data inputed from the api call using axios
-  renderIngredient = ({ item }) => (
+  const onPressIngredient = (ingredient) => {
+    navigation.navigate("Ingredient", { ingredient });
+  };
+
+  const renderIngredient = ({ item }) => (
     <TouchableHighlight
       underlayColor="rgba(73,182,77,0.9)"
-      onPress={() => onPressIngredient(item[0])}
+      onPress={() => onPressIngredient(item)}
     >
       <View style={styles.container}>
-        <Image style={styles.photo} source={{ uri: item[0].photo_url }} />
-        <Text style={styles.title}>{item[0].name}</Text>
-        <Text style={{ color: "grey" }}>{item[1]}</Text>
+        <Image style={styles.photo} source={{ uri: item.image }} />
+        <Text style={styles.title}>{item.name}</Text>
       </View>
     </TouchableHighlight>
   );
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.searchContainer}>
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Search for an ingredient"
-            onChangeText={(text) => this.setState({ search: text })}
-            value={this.state.search}
-          />
-          <Button
-            title="Search"
-            color="#0D0C0C"
-            backgroundColor="#0D0C0C"
-            onPress={this.fetchData}
-          />
-        </View>
-        <FlatList
-          data={this.state.recipes}
-          renderItem={this.renderIngredient}
-          keyExtractor={(item) => item.id}
-          image={this.state.recipes.image}
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search for an ingredient"
+          onChangeText={(text) => setSearch(text)}
+          value={search}
         />
+        <Button title="Search" color="#0D0C0C" onPress={fetchData} />
       </View>
-    );
-  }
-}
+      <FlatList
+        data={ingredients}
+        renderItem={renderIngredient}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
 
-export default SearchScreen;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "top",
+//     justifyContent: "top",
+//     backgroundColor: "#fff",
+//   },
+//   searchContainer: {
+//     flexDirection: "row",
+//     alignItems: "top",
+//     marginBottom: 20,
+//   },
+//   searchInput: {
+//     flex: 1,
+//     height: 40,
+//     borderColor: "#ccc",
+//     borderWidth: 1,
+//     borderRadius: 5,
+//     paddingHorizontal: 10,
+//     marginRight: 10,
+//   },
+//   searchButton: {
+//     backgroundColor: "#0099ff",
+//     borderRadius: 5,
+//     paddingVertical: 10,
+//     paddingHorizontal: 20,
+//   },
+//   searchButtonText: {
+//     color: "#fff",
+//     fontSize: 16,
+//   },
+//   carouselContainer: {
+//     height: 300,
+//   },
+//   slide: {
+//     backgroundColor: "#fff",
+//     borderRadius: 5,
+//     padding: 20,
+//     shadowColor: "#000",
+//     shadowOffset: {
+//       width: 0,
+//       height: 2,
+//     },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 3.84,
+//     elevation: 5,
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     marginBottom: 10,
+//   },
+// });
+
+export default PantryScreen;
--- src/screens/Search/styles.js
+++ src/screens/Search/styles.js
@@ -1,46 +1,71 @@
-import { StyleSheet } from "react-native";
+// style screen for a pantry of ingredients and items in the home that can be used to make a recipe
+// needs style for search bar, list of items, and buttons
+// needs style for barcode item next to search box 
+// needs style for list of items in pantry
+// needs style for categories of items in pantry
+
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
   searchContainer: {
-    flexDirection: "row-reverse",
-    alignItems: "center",
-    backgroundColor: "#FFFCFC",
-    borderRadius: 100,
-    width: 300,
-    justifyContent: "space-around",
-    marginVertical: 10,
-    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
   },
   searchInput: {
-    backgroundColor: "#EDEDED",
-    color: "#000000",
-    width: 180,
-    height: 50,
+    flex: 1,
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
     paddingHorizontal: 10,
-    borderRadius: 25,
+    marginRight: 10,
   },
-  photo: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
+  searchButton: {
+    backgroundColor: '#0099ff',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  searchButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  carouselContainer: {
+    height: 300,
+  },
+  slide: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   title: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#9A1D1D",
-    marginTop: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
-  category: {
-    fontSize: 14,
-    color: "#9A1D1D",
-    marginTop: 10,
+  photo: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
   },
 });
 
 export default styles;
+
+
+
