33332111a9747ec87dd0d91b9cc065c4abcd1ce9
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-03 08:30:28 -0500

    Search Style sheet

--- src/screens/Search/SearchScreen.js
+++ src/screens/Search/SearchScreen.js
@@ -95,58 +95,5 @@ const PantryScreen = ({ navigation }) => {
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "top",
-//     justifyContent: "top",
-//     backgroundColor: "#fff",
-//   },
-//   searchContainer: {
-//     flexDirection: "row",
-//     alignItems: "top",
-//     marginBottom: 20,
-//   },
-//   searchInput: {
-//     flex: 1,
-//     height: 40,
-//     borderColor: "#ccc",
-//     borderWidth: 1,
-//     borderRadius: 5,
-//     paddingHorizontal: 10,
-//     marginRight: 10,
-//   },
-//   searchButton: {
-//     backgroundColor: "#0099ff",
-//     borderRadius: 5,
-//     paddingVertical: 10,
-//     paddingHorizontal: 20,
-//   },
-//   searchButtonText: {
-//     color: "#fff",
-//     fontSize: 16,
-//   },
-//   carouselContainer: {
-//     height: 300,
-//   },
-//   slide: {
-//     backgroundColor: "#fff",
-//     borderRadius: 5,
-//     padding: 20,
-//     shadowColor: "#000",
-//     shadowOffset: {
-//       width: 0,
-//       height: 2,
-//     },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 3.84,
-//     elevation: 5,
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: "bold",
-//     marginBottom: 10,
-//   },
-// });
 
 export default PantryScreen;
