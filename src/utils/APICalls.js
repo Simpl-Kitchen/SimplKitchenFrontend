a629d6a2e46c7caef697a763e9e7fc59fd19dfc8
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-05 11:52:56 -0500

    API Calls.js

--- src/utils/APICalls.js
+++ src/utils/APICalls.js
@@ -35,10 +35,36 @@ const getIngredientsByName = async (queryObject) => {
 
 const addIngredientToPantry = async (ingredient) => {
 
+    //console.log("Inside getIngredientsByName")
+
+    const userToken = await getToken();
+    console.log("User token: " + JSON.stringify(userToken));
+
+    const options = {
+        method: "POST",
+        url: "https://simplkitchenapi.onrender.com/api/v1/pantry",
+        //params: queryObject,
+        headers: {
+            "Authorization": `Bearer ${userToken}`
+        },
+        data: {
+            "ingredientId" : ingredient.ingredientId,
+            "ingredientName" : ingredient.ingredientName,
+            "pictureURL" : ingredient.pictureURL
+        } 
+        
+      };
+
+      try {
+        const response = await axios.request(options);
+        console.log("Successfully added ingredient:", response.data);
+      } catch (error) {
+        console.error("Error adding ingredient to pantry:", error);
+      }
 
 
 }
 
 
 
-module.exports = {loginSimplKitchen, getIngredientsByName}
+module.exports = {loginSimplKitchen, getIngredientsByName, addIngredientToPantry}
