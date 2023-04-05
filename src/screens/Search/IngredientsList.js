5fcbea4de589b2f4827b042a444506a4c1176be1
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-05 17:00:41 -0500

    Comments

--- src/screens/Search/IngredientsList.js
+++ src/screens/Search/IngredientsList.js
@@ -1,14 +1,27 @@
+// Import required modules from React and React Native
 import React from "react";
 import { View, FlatList } from "react-native";
+
+// Import custom styles for the IngredientsList component
 import styles from "./styles";
+
+// Import the Ingredient component
 import Ingredient from "./Ingredient";
 
+// Define the IngredientsList functional component
+// It takes three props:
+// - ingredients: An array of ingredient objects
+// - onPressIngredient: A callback function for handling the press event on an ingredient
+// - handleAddIngredient: A callback function for handling the addition of an ingredient to the pantry
 const IngredientsList = ({ ingredients, onPressIngredient, handleAddIngredient }) => {
+  // Define the renderItem function, which will be used by the FlatList component
   const renderItem = ({ item, index }) => {
+    // Only render items with even indices to display two items per row
     if (index % 2 === 0) {
+       // Determine the second item to display in the same row, if it exists
       const secondItem =
         index + 1 < ingredients.length ? ingredients[index + 1] : null;
-
+       // Render the row containing the two items (or just one, if the second item is null)
       return (
         <View style={styles.rowContainer}>
           <Ingredient
@@ -26,9 +39,11 @@ const IngredientsList = ({ ingredients, onPressIngredient, handleAddIngredient }
         </View>
       );
     }
+
+    // For odd indices, do not render anything
     return null;
   };
-
+  // Render the IngredientsList component using a FlatList
   return (
     <FlatList
       data={ingredients}
@@ -37,5 +52,5 @@ const IngredientsList = ({ ingredients, onPressIngredient, handleAddIngredient }
     />
   );
 };
-
+// Export the IngredientsList component for use in other components
 export default IngredientsList;
