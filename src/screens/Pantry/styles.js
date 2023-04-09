45c49f53d77142bd9fb3f6875a63d0a4221e237a
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-09 12:38:37 -0500

    Updated Pantry Stylesheet

--- src/screens/Pantry/styles.js
+++ src/screens/Pantry/styles.js
@@ -3,56 +3,48 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-    padding: 10,
+    backgroundColor: "#F7F7F7",
+    padding: 20,
   },
   itemContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginBottom: 10,
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
   },
   photo: {
-    width: 50,
-    height: 50,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
     marginRight: 10,
   },
   title: {
-    fontSize: 18,
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  amount: {
+    fontSize: 16,
+    color: "#666",
   },
   emptyContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    height: 300,
   },
   emptyText: {
-    fontSize: 18,
-    fontStyle: "italic",
+    fontSize: 20,
+    color: "#666",
   },
   buttonContainer: {
     alignItems: "center",
-    marginVertical: 10,
-  },
-  ingredientsContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    padding: 10,
-  },
-  ingredientContainer: {
-    width: '48%',
-    marginBottom: 10,
-  },
-  // photo: {
-  //   width: '100%',
-  //   height: 150,
-  //   marginBottom: 5,
-  // },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    justifyContent: "center",
+    marginVertical: 20,
   },
 });
 
