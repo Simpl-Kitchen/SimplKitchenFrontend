bb0ebe8a1641083c7b9f8310d2a064d0a24896ff
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-08 21:55:32 -0500

    Edited babel.config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "dotenv-import",
+        {
+          moduleName: "@env",
+          path: "./.env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: false,
+        },
+      ],
+    ],
   };
 };
