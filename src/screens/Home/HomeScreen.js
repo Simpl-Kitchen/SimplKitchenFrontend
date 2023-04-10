cfb135dd463d1a9dd3e49fe595eccdaa50bfeaec
jace <jacegrimes@gmail.com>
2023-04-09 20:55:29 -0500

    home screen changes to display recipes

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import DrawerContainer from '../DrawerContainer/DrawerContainer.js';
+import styles from "./styles";
 
 const HomeScreen = ({ navigation }) => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=YOUR_API_KEY')
+      .then(response => response.json())
+      .then(data => setRecipes(data.recipes))
+      .catch(error => console.error(error));
+  }, []);
+
+  const renderRecipe = ({ item }) => {
+    return (
+      <View style={styles.recipe}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.summary}>{item.summary}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <DrawerContainer navigation={navigation} />
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={recipes}
+          renderItem={renderRecipe}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
     </View>
   );
 };
+
 
 export default HomeScreen;
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,11 +1,63 @@
-import { StyleSheet } from 'react-native';
-import { RecipeCard } from '../../AppStyles';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import DrawerContainer from '../DrawerContainer/DrawerContainer.js';
+
+const HomeScreen = ({ navigation }) => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=YOUR_API_KEY')
+      .then(response => response.json())
+      .then(data => setRecipes(data.recipes))
+      .catch(error => console.error(error));
+  }, []);
+
+  const renderRecipe = ({ item }) => {
+    return (
+      <View style={styles.recipe}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.summary}>{item.summary}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <DrawerContainer navigation={navigation} />
+      <View style={styles.content}>
+        <FlatList
+          data={recipes}
+          renderItem={renderRecipe}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: RecipeCard.container,
-  photo: RecipeCard.photo,
-  title: RecipeCard.title,
-  category: RecipeCard.category
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+  },
+  recipe: {
+    marginBottom: 16,
+    paddingBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  summary: {
+    fontSize: 16,
+  },
 });
 
-export default styles;
+export default HomeScreen;
