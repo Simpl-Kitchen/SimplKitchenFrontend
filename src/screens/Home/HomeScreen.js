477e282e64132e619b2d29da6cd01c7f9d14be1b
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-10 07:36:50 -0500

    Updates home screen to display randomized recipes

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,40 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import DrawerContainer from '../DrawerContainer/DrawerContainer.js';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, StyleSheet, ScrollView, Image } from "react-native";
 import styles from "./styles";
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=YOUR_API_KEY')
-      .then(response => response.json())
-      .then(data => setRecipes(data.recipes))
-      .catch(error => console.error(error));
+    fetch(
+      "https://api.spoonacular.com/recipes/random?number=10&apiKey=e44c9f0796b4400ab3a69f1354d139a9"
+    )
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.recipes))
+      .catch((error) => console.error(error));
   }, []);
 
   const renderRecipe = ({ item }) => {
     return (
       <View style={styles.recipe}>
+        <Image
+          source={{ uri: item.image }}
+          style={styles.image}
+          resizeMode="cover"
+        />
         <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.summary}>{item.summary}</Text>
+        <Text style={styles.summary}>
+          {item.summary.replace(/(<([^>]+)>)/gi, "")}
+        </Text>
+        <Text style={styles.info}>
+          Ready in {item.readyInMinutes} minutes | Serves {item.servings} |
+          Difficulty {item.difficulty}
+        </Text>
+        <Text style={styles.ingredientsTitle}>Ingredients:</Text>
+        <Text style={styles.ingredients}>
+          {item.extendedIngredients.map((i) => "- " + i.original).join("\n")}
+        </Text>
+        <Text style={styles.instructionsTitle}>Instructions:</Text>
+        <Text style={styles.instructions}>
+          {item.instructions
+            .replace(/(<([^>]+)>)/gi, "")
+            .replace(/\r?\n|\r/g, "\n\n")}
+        </Text>
       </View>
     );
   };
 
   return (
     <View style={{ flex: 1 }}>
-      <DrawerContainer navigation={navigation} />
-      <View style={{ flex: 1 }}>
-        <FlatList
-          data={recipes}
-          renderItem={renderRecipe}
-          keyExtractor={item => item.id.toString()}
-        />
-      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ padding: 16 }}>
+          <FlatList
+            data={recipes}
+            renderItem={renderRecipe}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
-
 
 export default HomeScreen;
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,63 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import DrawerContainer from '../DrawerContainer/DrawerContainer.js';
-
-const HomeScreen = ({ navigation }) => {
-  const [recipes, setRecipes] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=YOUR_API_KEY')
-      .then(response => response.json())
-      .then(data => setRecipes(data.recipes))
-      .catch(error => console.error(error));
-  }, []);
-
-  const renderRecipe = ({ item }) => {
-    return (
-      <View style={styles.recipe}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.summary}>{item.summary}</Text>
-      </View>
-    );
-  };
-
-  return (
-    <View style={styles.container}>
-      <DrawerContainer navigation={navigation} />
-      <View style={styles.content}>
-        <FlatList
-          data={recipes}
-          renderItem={renderRecipe}
-          keyExtractor={item => item.id.toString()}
-        />
-      </View>
-    </View>
-  );
-};
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
-    padding: 16,
+    backgroundColor: "#fff",
   },
   recipe: {
-    marginBottom: 16,
-    paddingBottom: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    backgroundColor: "#f2f2f2",
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 8,
+    overflow: "hidden",
+    padding: 16,
+  },
+  image: {
+    width: "100%",
+    height: 200,
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 8,
   },
   summary: {
     fontSize: 16,
+    marginBottom: 16,
+  },
+  info: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  ingredientsTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  ingredients: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  instructionsTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  instructions: {
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 16,
   },
 });
 
-export default HomeScreen;
+export default styles;
