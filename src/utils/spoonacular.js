44835d58749c5deb9c97b8637c70ad0531c71cb2
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-10 17:27:48 -0500

    Shell

---
+++ src/utils/spoonacular.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const searchIngredientsSpoonacular = async (query) => {
+    options = {
+        'query': query, // String | The (natural language) search query.
+        'addChildren': true, // Boolean | Whether to add children of found foods.
+        // 'metaInformation': false, // Boolean | Whether to return more meta information about the ingredients.
+        // 'intolerances': "egg", // String | A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
+        'intolerances': queryObject.intolerances, // String | A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
+        // 'sort': "calories", // String | The strategy to sort recipes by. See a full list of supported sorting options.
+        'sortDirection': "asc", // String | The direction in which to sort. Must be either 'asc' (ascending) or 'desc' (descending).
+        // 'offset': 56, // Number | The number of results to skip (between 0 and 900).
+        'number': 10, // Number | The maximum number of items to return (between 1 and 100). Defaults to 10.
+        // 'language': "en" // String | The language of the input. Either 'en' or 'de'.
+    };
+
+    
+}
+
+module.exports = { searchIngredientsSpoonacular };
