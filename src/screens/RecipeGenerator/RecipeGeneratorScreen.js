cc85b7e6a77c4f2cb8ee70d1746ebc31b62c6827
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-12 11:34:48 -0500

    Recipe Generator Style Update

--- src/screens/RecipeGenerator/RecipeGeneratorScreen.js
+++ src/screens/RecipeGenerator/RecipeGeneratorScreen.js
@@ -3,9 +3,12 @@
 import React from "react";
 import { View, Button, StyleSheet, Text } from "react-native";
 
+import styles from "./styles";
+
 const RecipeGeneratorScreen = () => {
   const handleGenerateRecipe = () => {
     // TODO: Implement recipe generation logic
+    
   };
 
   return (
@@ -30,28 +33,4 @@ const RecipeGeneratorScreen = () => {
 
 export default RecipeGeneratorScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingHorizontal: 20,
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 20,
-    marginBottom: 10,
-  },
-  headerTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  ButtonContainer: {
-    backgroundColor: "#A8DDA8",
-  },
-  Button: {
-    backgroundColor: "#A8DDA8",
-    borderRadius: 10,
-  },
-});
+
---
+++ src/screens/RecipeGenerator/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      paddingHorizontal: 20,
+    },
+    header: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 20,
+      marginBottom: 10,
+    },
+    headerTitle: {
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    ButtonContainer: {
+      backgroundColor: "#A8DDA8",
+    },
+    Button: {
+      backgroundColor: "#A8DDA8",
+      borderRadius: 10,
+    },
+  });
+
+  export default styles;
