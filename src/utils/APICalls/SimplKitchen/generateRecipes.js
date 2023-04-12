703aece49142ee595a4a102148d0afc2decbed97
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-12 15:31:20 -0500

    Generate recipes

--- src/utils/APICalls/SimplKitchen/generateRecipes.js
+++ src/utils/APICalls/SimplKitchen/generateRecipes.js
@@ -3,7 +3,22 @@ const { getToken } = require("../../AsyncStorage/userToken");
 import { SIMPLKITCHEN_API_URL } from "@env";
 
 const generateUserRecipes = async () => {
+    
     console.log("Generating recipes...")
+    const userToken = await getToken();
+        console.log(userToken)
+
+        const options = {
+            method: "GET",
+            url: `${SIMPLKITCHEN_API_URL}/generate/recipes`,
+            //params: queryObject,
+            headers: {
+                Authorization: `Bearer ${userToken}`,
+            },
+        };
+
+        const response = await axios.request(options);
+        return response;
 }
 
 const getGeneratedRecipes = async () => {
