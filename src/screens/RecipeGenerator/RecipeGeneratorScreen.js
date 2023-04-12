edab5738b3052a45c1f6ddd8b1e067bdc54e25ca
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-12 10:31:35 -0500

    Recipe Generator

--- src/screens/RecipeGenerator/RecipeGeneratorScreen.js
+++ src/screens/RecipeGenerator/RecipeGeneratorScreen.js
@@ -1,7 +1,7 @@
 // this will be a screen with a button at the top that says generate recipe and nothing else
 
-import React from 'react';
-import { View, Button } from 'react-native';
+import React from "react";
+import { View, Button, StyleSheet, Text } from "react-native";
 
 const RecipeGeneratorScreen = () => {
   const handleGenerateRecipe = () => {
@@ -9,11 +9,49 @@ const RecipeGeneratorScreen = () => {
   };
 
   return (
-    <View>
-      <Button title="Generate Recipe" onPress={handleGenerateRecipe} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Recipe Generator</Text>
+        <Text style={styles.clearButton} onPress={handleGenerateRecipe}>
+          Generate Recipe
+        </Text>
+      </View>
+      <View style={styles.ButtonContainer}>
+        <Button
+          style = {styles.Button}
+          title="Generate Recipe"
+          onPress={handleGenerateRecipe}
+          color="#A8DDA8"
+        />
+      </View>
     </View>
   );
 };
 
 export default RecipeGeneratorScreen;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  ButtonContainer: {
+    backgroundColor: "#A8DDA8",
+  },
+  Button: {
+    backgroundColor: "#A8DDA8",
+    borderRadius: 10,
+  },
+});
