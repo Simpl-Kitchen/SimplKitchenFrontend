64d53cc881d4bd3c416145be50123cd44c03e216
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-17 05:35:15 -0500

    added search for recipe screen

--- src/screens/RecipeGenerator/RecipeGeneratorScreen.js
+++ src/screens/RecipeGenerator/RecipeGeneratorScreen.js
@@ -1,62 +1,128 @@
-// this will be a screen with a button at the top that says generate recipe and nothing else
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  SafeAreaView,
+  RefreshControl,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { SearchBar } from "react-native-elements";
+import MenuButton from "../../components/MenuButton/MenuButton";
+import styles from "./styles";
 
-import React from "react";
-import { View, Button, StyleSheet, Text } from "react-native";
+const RecipeScreen = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [sortBy, setSortBy] = useState(null);
+  const [search, setSearch] = useState("");
+  const navigation = useNavigation();
 
-import {generateUserRecipes, getGeneratedRecipes} from "../../utils/APICalls/SimplKitchen/generateRecipes";
-
-const RecipeGeneratorScreen = () => {
-  const handleGenerateRecipe = async () => {
-    // TODO: Implement recipe generation logic
-    console.log("Clicked")
-    const response = await generateUserRecipes()
-    console.log(response)
+  const fetchData = (searchParam) => {
+    setRefreshing(true);
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${searchParam}&number=10&apiKey=e44c9f0796b4400ab3a69f1354d139a9`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data.results);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setRefreshing(false);
+      });
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Recipe Generator</Text>
-        <Text style={styles.clearButton} onPress={handleGenerateRecipe}>
-          Generate Recipe
-        </Text>
-      </View>
-      <View style={styles.ButtonContainer}>
-        <Button
-          style = {styles.Button}
-          title="Generate Recipe"
-          onPress={handleGenerateRecipe}
-          color="#A8DDA8"
+  useEffect(() => {
+    fetchData(""); // Fetch all recipes initially
+  }, []);
+
+  const handleSearch = () => {
+    fetchData(search);
+  };
+
+  useEffect(() => {
+    fetchData();
+    navigation.setOptions({
+      drawerLockMode: "locked-closed",
+      headerLeft: () => (
+        <MenuButton
+          title="Menu"
+          source={require("../../../assets/icons/menu.png")}
+          onPress={() => {
+            navigation.openDrawer();
+          }}
         />
+      ),
+      headerRight: () => (
+        <TouchableOpacity
+          style={styles.filterButton}
+          onPress={() => {
+            setSortBy((prevSortBy) => (prevSortBy === "asc" ? "desc" : "asc"));
+          }}
+        >
+          <Text style={styles.filterButtonText}>price ⇅</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, []); // Remove sortBy dependency to disable sorting
+
+
+
+  const renderRecipe = ({ item }) => {
+    return (
+      <View style={styles.recipe}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: item.image }} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          {/* <Text style={styles.info}>Likes: {item.aggregateLikes} </Text>
+          <Text style={styles.info}>Servings: {item.servings}</Text>
+          <Text style={styles.info}>
+            Ready in: {item.readyInMinutes} minutes
+          </Text>
+          <Text style={styles.info}>
+            Price per serving: ${(item.pricePerServing / 100).toFixed(2)}
+          </Text> */}
+        </View>
       </View>
-    </View>
+    );
+  };
+
+  const sortedRecipes = recipes.sort((a, b) => {
+    if (sortBy === "asc") {
+      return a.pricePerServing - b.pricePerServing;
+    } else if (sortBy === "desc") {
+      return b.pricePerServing - a.pricePerServing;
+    } else {
+      return 0;
+    }
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <SearchBar
+        placeholder="Search for recipes..."
+        onChangeText={setSearch}
+        value={search}
+        onSubmitEditing={handleSearch}
+        containerStyle={styles.searchBarContainer}
+        inputContainerStyle={styles.searchBarInputContainer}
+        lightTheme
+      />
+      <FlatList
+        data={sortedRecipes}
+        renderItem={renderRecipe}
+        keyExtractor={(item) => item.id}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={fetchData} />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
-export default RecipeGeneratorScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingHorizontal: 20,
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 20,
-    marginBottom: 10,
-  },
-  headerTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  ButtonContainer: {
-    backgroundColor: "#A8DDA8",
-  },
-  Button: {
-    backgroundColor: "#A8DDA8",
-    borderRadius: 10,
-  },
-});
+export default RecipeScreen;
---
+++ src/screens/RecipeGenerator/styles.js
@@ -0,0 +1,117 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0EFEF",
+  },
+  recipe: {
+    flex: 1,
+    flexDirection: "column",
+    marginHorizontal: 20,
+    marginVertical: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+  imageContainer: {
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+  },
+  textContainer: {},
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  info: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  summary: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  ingredientsTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  ingredients: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  instructionsTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  instructions: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  addButton: {
+    backgroundColor: "#22aa88",
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+    alignSelf: "flex-start",
+  },
+  buttonContainer: {
+    alignSelf: "flex-end",
+    marginTop: 10,
+    backgroundColor: "#B0B4B33A",
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+  },  
+  addButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  filterButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 10,
+    borderRadius: 20,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+  filterButton:{
+    backgroundColor: "#A8DDA8",
+    borderRadius: 20,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+  searchBarContainer: {
+    backgroundColor: "transparent",
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    paddingHorizontal: 8,
+  },
+
+  searchBarInputContainer: {
+    backgroundColor: "#f0f0f0",
+  },
+});
+
+export default styles;
