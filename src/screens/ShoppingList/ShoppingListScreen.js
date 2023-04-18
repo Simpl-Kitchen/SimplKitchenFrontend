0b04f4f04e422d9d08bd7f33731e467bce9b1f46
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-04-18 07:40:42 -0500

    Shopping list inputted

--- src/screens/ShoppingList/ShoppingListScreen.js
+++ src/screens/ShoppingList/ShoppingListScreen.js
@@ -2,63 +2,109 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
+  TextInput,
   TouchableOpacity,
   FlatList,
-  ScrollView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import styles from "./styles";
-import { useNavigation } from "@react-navigation/native";
-
-import axios from "axios";
 
 const ShoppingListScreen = () => {
+  const [inputText, setInputText] = useState("");
   const [shoppingList, setShoppingList] = useState([]);
-  const navigation = useNavigation();
 
-  const apiKey = "your_api_key_here";
-  const username = "your_username_here";
-  const startDate = "2023-04-17";
-  const endDate = "2023-04-23";
-  const apiURL = `https://api.spoonacular.com/mealplanner/${username}/shopping-list/${startDate}/${endDate}?apiKey=${apiKey}`;
-
+  // Load saved items from AsyncStorage on component mount
   useEffect(() => {
-    fetchShoppingList();
+    async function loadItems() {
+      const savedItems = await AsyncStorage.getItem("shoppingList");
+      if (savedItems) {
+        setShoppingList(JSON.parse(savedItems));
+      }
+    }
+    loadItems();
   }, []);
 
-  const fetchShoppingList = async () => {
-    try {
-      const response = await axios.get(apiURL);
-      setShoppingList(response.data.ingredients);
-    } catch (error) {
-      console.error("Error fetching shopping list:", error);
+  // Save items to AsyncStorage when the shoppingList state changes
+  useEffect(() => {
+    async function saveItems() {
+      await AsyncStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+    }
+    saveItems();
+  }, [shoppingList]);
+
+  const handleAddItem = () => {
+    if (inputText.trim() !== "") {
+      setShoppingList([
+        ...shoppingList,
+        { id: Date.now(), text: inputText, checked: false },
+      ]);
+      setInputText("");
     }
   };
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity
-      style={styles.shoppingListItem}
-      onPress={() => {
-        navigation.navigate("IngredientScreen", { ingredient: item });
-      }}
-    >
-      <Text style={styles.ingredientName}>{item.name}</Text>
-      <Text style={styles.ingredientAmount}>
-        {item.amount.metric.value} {item.amount.metric.unit}
-      </Text>
-    </TouchableOpacity>
-  );
+  const handleRemoveItem = (itemId) => {
+    const updatedList = shoppingList.filter((item) => item.id !== itemId);
+    setShoppingList(updatedList);
+  };
+
+  const handleToggleCheck = (itemId) => {
+    const updatedList = shoppingList.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, checked: !item.checked };
+      }
+      return item;
+    });
+    setShoppingList(updatedList);
+  };
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <FlatList
-          data={shoppingList}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={renderItem}
+    <View style={{ flex: 1, padding: 16 }}>
+      <View style={{ flexDirection: "row", marginBottom: 16 }}>
+        <TextInput
+          value={inputText}
+          onChangeText={setInputText}
+          placeholder="Add an item"
+          style={{
+            flex: 1,
+            marginRight: 16,
+            padding: 8,
+            backgroundColor: "#fff",
+            borderRadius: 8,
+          }}
         />
+        <TouchableOpacity
+          onPress={handleAddItem}
+          style={{ backgroundColor: "#A8DDA8", padding: 8, borderRadius: 8 }}
+        >
+          <Text style={{ color: "#fff" }}>Add</Text>
+        </TouchableOpacity>
       </View>
-    </ScrollView>
+      <FlatList
+        data={shoppingList}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 8,
+            }}
+            onPress={() => handleToggleCheck(item.id)}
+          >
+            <Text
+              style={{
+                flex: 1,
+                textDecorationLine: item.checked ? "line-through" : "none",
+              }}
+            >
+              {item.text}
+            </Text>
+            <TouchableOpacity onPress={() => handleRemoveItem(item.id)}>
+              <Text style={{ color: "#FF3B30" }}>Remove</Text>
+            </TouchableOpacity>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
   );
 };
 
--- src/screens/ShoppingList/styles.js
+++ src/screens/ShoppingList/styles.js
@@ -1,20 +1,23 @@
-import { StyleSheet } from "react-native";
+// THis is the stylesheet for the working API request on the APIShopingList.js file
 
-export default StyleSheet.create({
-  // ...
-  shoppingListItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  ingredientName: {
-    fontSize: 18,
-  },
-  ingredientAmount: {
-    fontSize: 16,
-    color: "#777",
-  },
-});
+
+// import { StyleSheet } from "react-native";
+
+// export default StyleSheet.create({
+//   // ...
+//   shoppingListItem: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     padding: 10,
+//     borderBottomWidth: 1,
+//     borderBottomColor: "#ccc",
+//   },
+//   ingredientName: {
+//     fontSize: 18,
+//   },
+//   ingredientAmount: {
+//     fontSize: 16,
+//     color: "#777",
+//   },
+// });
