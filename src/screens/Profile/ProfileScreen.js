02d093cfc6da288aa0b077052948b497a49f84ec
Scott Houghton <94723197+EdwardHoughton@users.noreply.github.com>
2023-04-19 13:59:39 -0500

    Added Food Prefrences to the profile screen
    Hey hammad lol

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,3 +1,78 @@
+// import React, { useState } from "react";
+// import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+// import { MultipleSelectList } from 'react-native-dropdown-select-list'
+
+// import { updateUserIntolerences, logoutSimplKitchen } from '../../utils/APICalls/SimplKitchen/user'
+
+// import styles from "./styles";
+
+// export default function ProfileScreen(props) {
+
+//   const [selected, setSelected] = React.useState([]);
+
+
+//   const data = [
+//     { key: '1', value: 'Dairy' },//, disabled: true },
+//     { key: '2', value: 'Egg' },
+//     { key: '3', value: 'Gluten' },
+//     { key: '4', value: 'Grain' },//, disabled: true },
+//     { key: '5', value: 'Peanut' },
+//     { key: '6', value: 'Seafood' },
+//     { key: '7', value: 'Sesame' },
+//     { key: '8', value: 'Shellfish' },
+//     { key: '9', value: 'Soy' },
+//     { key: '10', value: 'Sulfite' },
+//     { key: '11', value: 'Tree Nut' },
+//     { key: '12', value: 'Wheat' },
+//   ]
+
+//   const handleSave = () => {
+//     console.log(selected)
+
+//     try {
+//       updateUserIntolerences(selected)
+//     } catch (error) {
+//       console.log(error)
+//     }
+
+//     alert("Saved selected items")
+//   }
+
+//   const handleLogout = async () => {
+//     // Perform logout actions here
+//     console.log("Logout clicked");
+
+//     try {
+//       await logoutSimplKitchen()
+//     } catch (error) {
+
+//     }
+//   };
+
+
+//   return (
+//     <View style={styles.container}>
+//       <MultipleSelectList
+//         setSelected={(val) => setSelected(val)}
+//         data={data}
+//         save="value"
+//         onSelect={() => { }}
+//         label="Categories"
+//       />
+//       <View style={styles.buttonsContainer}>
+//         <TouchableOpacity style={styles.button} onPress={handleSave}>
+//           <Text style={styles.buttonText}>Save</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+//           <Text style={styles.buttonText}>Logout</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+
+// }
+
+
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { MultipleSelectList } from 'react-native-dropdown-select-list'
@@ -8,10 +83,10 @@ import styles from "./styles";
 
 export default function ProfileScreen(props) {
 
-  const [selected, setSelected] = React.useState([]);
+  const [selected1, setSelected1] = React.useState([]);
+  const [selected2, setSelected2] = React.useState([]);
 
-
-  const data = [
+  const data1 = [
     { key: '1', value: 'Dairy' },//, disabled: true },
     { key: '2', value: 'Egg' },
     { key: '3', value: 'Gluten' },
@@ -26,11 +101,19 @@ export default function ProfileScreen(props) {
     { key: '12', value: 'Wheat' },
   ]
 
+  const data2 = [
+    { key: '1', value: 'Vegetarian' },
+    { key: '2', value: 'Vegan' },
+    { key: '3', value: 'Pescetarian' }, 
+    { key: '4', value: 'Halal' },
+  ]
+
   const handleSave = () => {
-    console.log(selected)
+    console.log(selected1)
+    console.log(selected2)
 
     try {
-      updateUserIntolerences(selected)
+      updateUserIntolerences(selected1.concat(selected2))
     } catch (error) {
       console.log(error)
     }
@@ -53,11 +136,18 @@ export default function ProfileScreen(props) {
   return (
     <View style={styles.container}>
       <MultipleSelectList
-        setSelected={(val) => setSelected(val)}
-        data={data}
+        setSelected={(val) => setSelected1(val)}
+        data={data1}
         save="value"
         onSelect={() => { }}
-        label="Categories"
+        label="Intolerances"
+      />
+      <MultipleSelectList
+        setSelected={(val) => setSelected2(val)}
+        data={data2}
+        save="value"
+        onSelect={() => { }}
+        label="Options"
       />
       <View style={styles.buttonsContainer}>
         <TouchableOpacity style={styles.button} onPress={handleSave}>
@@ -71,6 +161,7 @@ export default function ProfileScreen(props) {
   );
 
 }
+
 
 // const styles = StyleSheet.create({
 //   container: {
