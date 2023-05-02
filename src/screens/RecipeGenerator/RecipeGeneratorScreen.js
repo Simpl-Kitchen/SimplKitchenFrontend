6eb992cbaebb4c8c1b5c9cfd260d7ddc6e7d35c6
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-05-02 12:51:55 -0500

    Add loding icon when button pushed

--- src/screens/RecipeGenerator/RecipeGeneratorScreen.js
+++ src/screens/RecipeGenerator/RecipeGeneratorScreen.js
@@ -1,64 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { Image, View, Text, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator } from "react-native";
 import axios from "axios";
 
 import GenerateStyles from "./styles";
 
 import { generateUserRecipes, getGeneratedRecipes } from "../../utils/APICalls/SimplKitchen/generateRecipes";
 
-
-
-//RecipeCard that takes a single prop called recipe. 
-//The component returns a view with two Text components, one displaying the recipe title and one displaying a 
-//comma-separated list of the used ingredients in the recipe. 
-  //The styles for the view and Text components are defined elsewhere in the code.
 const RecipeCard = ({ recipe }) => {
   return (
     <View style={styles.recipeCard}>
+      <Image style={styles.recipeImage} source={{ uri: recipe.image }} />
       <Text style={styles.recipeTitle}>{recipe.title}</Text>
       <Text style={styles.recipeIngredients}>{recipe.usedIngredients.join(", ")}</Text>
     </View>
   );
 };
 
-
-
-  //functional component called RecipeGeneratorScreen. 
-  //It uses the useState hook to manage an array of recipe objects, 
-  //and the useEffect hook to fetch generated recipes when the component mounts. 
-  //The fetchGeneratedRecipes function is used to make an asynchronous API call to get generated recipes and 
-  //update the state of the component with the fetched recipes. 
-  //The handleGenerateRecipes function is used to generate user recipes when the "Generate Recipes" button is pressed
-  // and the recipes state is rendered as a list of RecipeCard components inside a ScrollView.
 const RecipeGeneratorScreen = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-
-
-
-  //an asynchronous function named fetchGeneratedRecipes which retrieves generated recipes using the getGeneratedRecipes 
-  //function and updates the state by setting the fetched recipes using the setRecipes function. 
-  //If an error occurs during the fetching process, the error message is logged to the console.
   const fetchGeneratedRecipes = async () => {
     try {
-      const generatedRecipes = await getGeneratedRecipes(); // Get generated recipes
-      setRecipes(generatedRecipes.queue); // Update the state with the fetched recipes
+      const generatedRecipes = await getGeneratedRecipes();
+      setRecipes(generatedRecipes.queue);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching recipes:", error);
     }
   };
 
-
-
-
-  //A function called handleGenerateRecipes that is marked as an asynchronous function with the async keyword. 
-  //Within this function, there is a try-catch block that attempts to execute a function called generateUserRecipes()
-  //using the await keyword. If there is an error, it is caught and logged to the console with console.error().
   const handleGenerateRecipes = async () => {
+    setLoading(true);
     try {
-      await generateUserRecipes(); // Generate user recipes
+      await generateUserRecipes();
+      fetchGeneratedRecipes();
     } catch (error) {
       console.error("Error generating recipes:", error);
+      setLoading(false);
     }
   };
 
@@ -68,14 +47,16 @@ const RecipeGeneratorScreen = () => {
 
   return (
     <ScrollView>
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handleGenerateRecipes}>
-        <Text style={styles.buttonText}>Generate Recipes</Text>
-      </TouchableOpacity>
-      {recipes.map((recipe, index) => (
-        <RecipeCard key={index} recipe={recipe} />
-      ))}
-    </View>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={handleGenerateRecipes}>
+          <Text style={styles.buttonText}>Generate Recipes</Text>
+        </TouchableOpacity>
+        {loading ? (
+          <ActivityIndicator size="large" color="#97DF99" style={styles.loadingIndicator} />
+        ) : (
+          recipes.map((recipe, index) => <RecipeCard key={index} recipe={recipe} />)
+        )}
+      </View>
     </ScrollView>
   );
 };
@@ -104,13 +85,22 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 10,
   },
-  recipeTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
+recipeImage: {
+  width: "100%",
+  height: 200,
+  resizeMode: "cover",
+  marginBottom: 10,
+},
+recipeTitle: {
+  fontSize: 24,
+  fontWeight: "bold",
+},
   recipeIngredients: {
     fontSize: 16,
     marginTop: 10,
+  },
+  loadingIndicator: {
+    marginTop: 30,
   },
 });
 
