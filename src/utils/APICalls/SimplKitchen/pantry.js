4d08352e9a37208fc04b86297b9c7acb0adc5250
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-05-03 14:57:19 -0500

    Put user recipe utils in their own file

--- src/utils/APICalls/SimplKitchen/pantry.js
+++ src/utils/APICalls/SimplKitchen/pantry.js
@@ -108,83 +108,9 @@ const updateIngredientAmount = async (ingredient) => {
     }
 }
 
-// Use add recipes for the save button on the generate recipe page
-const addRecipe = async (recipe) => {
-    try {
-
-        const userToken = await getToken();
-
-        console.log("User token: " + JSON.stringify(userToken));
-
-        const options = {
-            method: "POST",
-            url: `${SIMPLKITCHEN_API_URL}/pantry/recipe`,
-            headers: {
-                Authorization: `Bearer ${userToken}`,
-            },
-            data: recipe,
-        };
-
-        const response = await axios.request(options);
-        console.log("Successfully added recipe:", response.data);
-    } catch (error) {
-        console.error("Error adding recipe to pantry:", error);
-    }
-}
-// Use get recipes on the user recipe page
-const getRecipes = async () => {
-    try {
-
-        const userToken = await getToken();
-
-        console.log("User token: " + JSON.stringify(userToken));
-
-        const options = {
-            method: "GET",
-            url: `${SIMPLKITCHEN_API_URL}/pantry/recipe`,
-            headers: {
-                Authorization: `Bearer ${userToken}`,
-            },
-            data: recipe,
-        };
-
-        const response = await axios.request(options);
-        console.log("Successfully added recipe:", response.data);
-    } catch (error) {
-        console.error("Error adding recipe to pantry:", error);
-    }
-}
-// Use delete recipe on the user recipe page to remove a recipe
-const deleteRecipe = async (recipe) => {
-    id = recipe._id
-    try {
-
-        const userToken = await getToken();
-
-        console.log("User token: " + JSON.stringify(userToken));
-
-        const options = {
-            method: "DELETE",
-            url: `${SIMPLKITCHEN_API_URL}/pantry/recipe/${id}`,
-            headers: {
-                Authorization: `Bearer ${userToken}`,
-            },
-        };
-
-        const response = await axios.request(options);
-        console.log("Successfully added recipe:", response.data);
-    } catch (error) {
-        console.error("Error adding recipe to pantry:", error);
-    }
-}
-
-
 module.exports = {
     addIngredientToPantry,
     getUsersIngredients,
     removeIngredientFromPantry,
     updateIngredientAmount,
-    addRecipe,
-    getRecipes,
-    deleteRecipe,
 }
