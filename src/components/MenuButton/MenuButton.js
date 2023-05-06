5ab9e0238d911b9e5c28bf94f65c09e07ec44f63
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-05-06 12:35:28 -0500

    Fixed the menu button being required and reapplied the icon

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -2,13 +2,14 @@ import React from "react";
 import { TouchableOpacity, Text, View } from "react-native";
 import PropTypes from "prop-types";
 import styles from "./styles";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MenuButton(props) {
   const { title, icon, onPress } = props;
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.button}>
-      <View style={styles.icon}>{icon}</View>
+        {icon && <View style={styles.icon}>{icon}</View>}
         <Text style={styles.text}>{title}</Text>
       </View>
     </TouchableOpacity>
@@ -18,5 +19,9 @@ export default function MenuButton(props) {
 MenuButton.propTypes = {
   title: PropTypes.string.isRequired,
   onPress: PropTypes.func.isRequired,
-  icon: PropTypes.element.isRequired,
+  icon: PropTypes.element,
+};
+
+MenuButton.defaultProps = {
+  icon: <Ionicons name="ios-menu" size={24} color="black" />,
 };
