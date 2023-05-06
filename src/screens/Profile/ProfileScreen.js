a393943b4068c86507978bb2a5ed33c56e4c63a1
Jace Grimes <79050342+JaceGrimes@users.noreply.github.com>
2023-05-06 14:04:30 -0500

    added the menu button to the header

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,69 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { MultipleSelectList } from 'react-native-dropdown-select-list'
-import { updateUserIntolerences, updateUserDiets, logoutSimplKitchen } from '../../utils/APICalls/SimplKitchen/user'
-
+import { MultipleSelectList } from "react-native-dropdown-select-list";
+import {
+  updateUserIntolerences,
+  updateUserDiets,
+  logoutSimplKitchen,
+} from "../../utils/APICalls/SimplKitchen/user";
+import { useNavigation } from "@react-navigation/native";
+import MenuButton from "../../components/MenuButton/MenuButton";
 import styles from "./styles";
 
 export default function ProfileScreen(props) {
-
-  const [selected1, setSelected1] = React.useState([]);
-  const [selected2, setSelected2] = React.useState([]);
+  const [selected1, setSelected1] = useState([]);
+  const [selected2, setSelected2] = useState([]);
+  const navigation = useNavigation();
 
   const data1 = [
-    { key: '1', value: 'Dairy' },//, disabled: true },
-    { key: '2', value: 'Egg' },
-    { key: '3', value: 'Gluten' },
-    { key: '4', value: 'Grain' },//, disabled: true },
-    { key: '5', value: 'Peanut' },
-    { key: '6', value: 'Seafood' },
-    { key: '7', value: 'Sesame' },
-    { key: '8', value: 'Shellfish' },
-    { key: '9', value: 'Soy' },
-    { key: '10', value: 'Sulfite' },
-    { key: '11', value: 'Tree Nut' },
-    { key: '12', value: 'Wheat' },
-  ]
+    { key: "1", value: "Dairy" }, //, disabled: true },
+    { key: "2", value: "Egg" },
+    { key: "3", value: "Gluten" },
+    { key: "4", value: "Grain" }, //, disabled: true },
+    { key: "5", value: "Peanut" },
+    { key: "6", value: "Seafood" },
+    { key: "7", value: "Sesame" },
+    { key: "8", value: "Shellfish" },
+    { key: "9", value: "Soy" },
+    { key: "10", value: "Sulfite" },
+    { key: "11", value: "Tree Nut" },
+    { key: "12", value: "Wheat" },
+  ];
 
   const data2 = [
-    { key: '1', value: 'Gluten Free' },
-    { key: '2', value: 'Ketogenic' },
-    { key: '3', value: 'Lacto-Vegetarian' },
-    { key: '4', value: 'Ovo-Vegetarian' },
-    { key: '5', value: 'Vegan' },
-    { key: '6', value: 'Pescetarian' },
-    { key: '7', value: 'Paleo' },
-    { key: '8', value: 'Primal' },
-    { key: '9', value: 'LowFODMAP' },
-    { key: '10', value: 'Whole30' },
-  ]
+    { key: "1", value: "Gluten Free" },
+    { key: "2", value: "Ketogenic" },
+    { key: "3", value: "Lacto-Vegetarian" },
+    { key: "4", value: "Ovo-Vegetarian" },
+    { key: "5", value: "Vegan" },
+    { key: "6", value: "Pescetarian" },
+    { key: "7", value: "Paleo" },
+    { key: "8", value: "Primal" },
+    { key: "9", value: "LowFODMAP" },
+    { key: "10", value: "Whole30" },
+  ];
 
   const handleSave = async () => {
-    // console.log("Hello")
-    // console.log(selected1)
-    // console.log(selected2)
-
     try {
-      await updateUserIntolerences(selected1)
-      await updateUserDiets(selected2)
+      await updateUserIntolerences(selected1);
+      await updateUserDiets(selected2);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
-    alert("Saved selected items")
-  }
-
-  const handleLogout = async () => {
-    // Perform logout actions here
-    console.log("Logout clicked");
-
-    try {
-      await logoutSimplKitchen()
-    } catch (error) {
-
-    }
+    alert("Saved selected items");
   };
 
+  const handleLogout = async () => {
+    console.log("Logout clicked");
+    try {
+      await logoutSimplKitchen();
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <MenuButton
+          title="Menu"
+          source={require("../../../assets/icons/menu.png")}
+          onPress={() => {
+            navigation.openDrawer();
+          }}
+        />
+      ),
+      drawerLockMode: "locked-closed",
+    });
+  }, [navigation]);
 
   return (
     <View style={styles.container}>
@@ -71,24 +81,24 @@ export default function ProfileScreen(props) {
         setSelected={(val) => setSelected1(val)}
         data={data1}
         save="value"
-        onSelect={() => { }}
+        onSelect={() => {}}
         label="Intolerances"
       />
       <MultipleSelectList
         setSelected={(val) => setSelected2(val)}
         data={data2}
-    save="value"
-    onSelect={() => { }}
-    label="Diets"
-  />
-  <View style={styles.buttonsContainer}>
-    <TouchableOpacity style={styles.button} onPress={handleSave}>
-      <Text style={styles.buttonText}>Save</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-      <Text style={styles.buttonText}>Logout</Text>
-    </TouchableOpacity>
-  </View>
-</View>
-);
+        save="value"
+        onSelect={() => {}}
+        label="Diets"
+      />
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleSave}>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
